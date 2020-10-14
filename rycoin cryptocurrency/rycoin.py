55b96ca8bde1f6eabc8fca6cc6e344a0c4b3f580
Ryan Strawser <strawser.ryan@gmail.com>
2020-10-13 17:38:56 -0700

    Added key implementation of transactions
    Integrate transactions that were accumulated in list of transactions just before they are added to the block.
    Make a request to add a new block to the blockchain with a POST request within Postman. Do this making a json file to post the json format into the response, tells us which block was added as well
    
    ** Added VARIABLE: node_address **
    - for whenever a miner mines a block they get transaction fees from that block
    - remembering the add_node method. uuid4() method will generate a unique address... NOTICE UUID function are generated as: {12345678-1234-5678-1234-567812345678} so we will want to replace the '-'s with nothing
    
    ** Added KEY: transactions to mining block section **

--- rycoin cryptocurrency/rycoin.py
+++ rycoin cryptocurrency/rycoin.py
@@ -125,6 +125,10 @@ class Blockchain:
 # Creating a Web App
 app = Flask(__name__)
 
+# Creating an address for the node on Port: 5000
+# replaces dashes with nothing
+node_address = str(uuid4()).replace('-', '')
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
@@ -135,12 +139,14 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, receiver = 'Ryan', amount = 1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']}
     return jsonify(response), 200
 
 # Getting the full Blockchain
